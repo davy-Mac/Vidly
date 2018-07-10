017e5f9598ffe381489a4a563958eab0c03873fd
David Mac <davidmmaceda@gmail.com>
2018-07-10 21:38:47 +0800

    Load Customers from Database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -21,7 +21,7 @@ namespace Vidly.Controllers
         }
 
         // GET: Customers
-        public ActionResult Index()
+        public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
