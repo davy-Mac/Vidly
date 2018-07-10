c1e52e6912102c157bae233959ae55232e4b6977
David Mac <davidmmaceda@gmail.com>
2018-07-10 15:32:45 +0800

    CodeFirstMigrations

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,32 +7,35 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context; //instance of ApplicationDbContext
+
+
+        public CustomersController() // initializing the context object
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing) // overrides the Dispose method of the base controller class
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
 
                 return HttpNotFound();
 
             return View(customer);
         }
-
-        IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-                {
-                    new Customer {Id = 1, Name = "John Smith"},
-                    new Customer {Id = 2, Name = "Mary Williams"}
-                };
-        }
-
     }
 }
