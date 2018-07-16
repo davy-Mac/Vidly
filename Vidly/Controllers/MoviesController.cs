d4d0e0ddf71902c61486f23d88402968a811175b
David Mac <davidmmaceda@gmail.com>
2018-07-16 10:36:04 +0800

    New Movie and edit form implemented and tested

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -28,16 +29,66 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
-        public ActionResult Details(int id)
+        public ActionResult New()
         {
-            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
             return View(movie);
 
         }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie) // method to add/create a new Movie
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+
 
         //GET: Movies/Random
         public ActionResult Random()
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,15 +9,22 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
+
         public Genre Genre { get; set; }
+
         [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
-        [Required]
+
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        [Required]
+
+
         public DateTime DateAdded { get; set; }
-        [Required]
+
+
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 
